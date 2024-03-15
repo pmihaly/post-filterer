362f9a5756381451457e749462bf8346bb9150cb
pmihaly <misi@pappmihaly.com>
2024-03-15 09:21:57 +0100

    add formatting

--- flake.nix
+++ flake.nix
@@ -3,19 +3,22 @@
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks, treefmt-nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        deps = with pkgs; [
-          go
-        ];
-        devDeps = with pkgs; [
-        ];
-      in
-      {
+        deps = with pkgs; [ go ];
+        devDeps = with pkgs; [ ];
+
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ({ pkgs, ... }: {
+          projectRootFile = "flake.nix";
+          programs.nixfmt.enable = true;
+          programs.gofmt.enable = true;
+        });
+      in {
         packages.default = pkgs.buildGoModule {
           name = "post-mixer";
           src = ./.;
@@ -27,15 +30,18 @@
           pre-commit-check = pre-commit-hooks.lib.${system}.run {
             src = ./.;
             hooks = {
-              nixpkgs-fmt.enable = true;
+              nixfmt.enable = true;
               gofmt.enable = true;
             };
           };
+          formatter = treefmtEval.config.build.check;
         };
 
         devShell = nixpkgs.legacyPackages.${system}.mkShell {
           inherit (self.checks.${system}.pre-commit-check) shellHook;
           buildInputs = deps ++ devDeps;
         };
+
+        formatter = treefmtEval.config.build.wrapper;
       });
 }
